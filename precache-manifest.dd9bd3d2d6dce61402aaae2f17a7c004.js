f82a60ab7694310fedde13fdcfdb60b3040bf3e8
jgauthier78@gmail.com <jgauthier78@gmail.com>
2021-08-16 15:08:20 +0200

    Updates

---
+++ precache-manifest.dd9bd3d2d6dce61402aaae2f17a7c004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d66506df636aba507cf33abbf7629b",
+    "url": "/simple-storage-react/index.html"
+  },
+  {
+    "revision": "1e05373554f633ce55f0",
+    "url": "/simple-storage-react/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "4426d38e716b86f60fa3",
+    "url": "/simple-storage-react/static/js/2.3ae4841c.chunk.js"
+  },
+  {
+    "revision": "1e05373554f633ce55f0",
+    "url": "/simple-storage-react/static/js/main.161ff594.chunk.js"
+  },
+  {
+    "revision": "72dcd4ef04db4c28b066",
+    "url": "/simple-storage-react/static/js/runtime-main.6d18673f.js"
+  }
+]);
